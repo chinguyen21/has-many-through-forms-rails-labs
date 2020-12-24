0d9e444cafc30343d3d6208c308abb535b469552
chinguyen21 <nguyen.thimai@northeastern.edu>
2020-12-23 19:01:31 -0500

    Better way using reject_if

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,12 +3,12 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
-  accepts_nested_attributes_for :categories
+  accepts_nested_attributes_for :categories, reject_if: proc {|attributes| attributes['name'].blank?}
 
-  def categories_attributes=(categories_attributes)
-    if categories_attributes.values[0][:name] != ""
-      self.categories << Category.create(name: categories_attributes.values[0][:name])
-    end
-  end
+  # def categories_attributes=(categories_attributes)
+  #   if categories_attributes.values[0][:name] != ""
+  #     self.categories << Category.create(name: categories_attributes.values[0][:name])
+  #   end
+  # end
 
 end
